6493403d5028cf87250e3028b8ff4e48308f634d
Conner D <connerdunn7399@gmail.com>
2017-11-18 14:57:46 -0700

    Added a top logo and 'DrillSgt' title

--- App.js
+++ App.js
@@ -1,13 +1,32 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <View style={{marginTop: 30, flexDirection: 'row'}}>
+          <View style={{flex: 1, padding: 5}}>
+            <Image
+              style={{flex:1, width:'100%'}}
+              resizeMode='contain'
+              source={require('./img/drill-sgt-logo.gif')}
+            />
+          </View>
+          <View style={{flex: 5}}>
+            <Text style={{fontSize: 69, textAlign: 'left', paddingLeft: 10}}>Drill Sgt.</Text>
+          </View>
+        </View>
+
+        <View style={{flexGrow: 8}}>
+          <Text>Open up App.js to start working on your app!</Text>
+          <Text>Changes you make will automatically reload.</Text>
+          <Text>Shake your phone to open the developer menu.</Text>
+        </View>
+
+        <View style={{flexGrow: 1}}>
+          <Text>Bottom buttons</Text>
+        </View>
       </View>
     );
   }
