e694691f0e1a80fc94b2149fde7f95e42aa60e7e
Conner D <connerdunn7399@gmail.com>
2017-11-18 16:26:33 -0700

    The Old DrillSgt Logo and Text are back!

--- App.js
+++ App.js
@@ -9,7 +9,8 @@ import {
   Platform,
   StyleSheet,
   Text,
-  View
+  View,
+  Image
 } from 'react-native';
 
 const instructions = Platform.select({
@@ -23,15 +24,34 @@ export default class App extends Component<{}> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+        <View style={{marginTop: 30, flexDirection: 'row'}}>
+          <View style={{flex: 1, padding: 5}}>
+            <Image
+              style={{flex:1, width:'100%'}}
+              resizeMode='contain'
+              source={require('./img/drill-sgt-logo.gif')}
+            />
+          </View>
+          <View style={{flex: 5}}>
+            <Text style={{fontSize: 69, textAlign: 'left', paddingLeft: 10}}>Drill Sgt.</Text>
+          </View>
+        </View>
+
+        <View style={{flexGrow: 8}}>
+          <Text style={styles.welcome}>
+            Welcome to React Native!
+          </Text>
+          <Text style={styles.instructions}>
+            To get started, edit App.js
+          </Text>
+          <Text style={styles.instructions}>
+            {instructions}
+          </Text>
+        </View>
+
+        <View style={{flexGrow: 1}}>
+          <Text>Bottom buttons</Text>
+        </View>
       </View>
     );
   }
@@ -40,9 +60,9 @@ export default class App extends Component<{}> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    backgroundColor: '#fff',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    justifyContent: 'center',
   },
   welcome: {
     fontSize: 20,
