d2cd0f7787879dc8aacd426ea76be7dab1589b61
Conner D <connerdunn7399@gmail.com>
2017-11-18 16:14:10 -0700

    Init the app better, there goes 4hrs ¯\_(ツ)_/¯

--- App.js
+++ App.js
@@ -1,32 +1,37 @@
-import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
 
-export default class App extends React.Component {
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' +
+    'Cmd+D or shake for dev menu',
+  android: 'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+export default class App extends Component<{}> {
   render() {
     return (
       <View style={styles.container}>
-        <View style={{marginTop: 30, flexDirection: 'row'}}>
-          <View style={{flex: 1, padding: 5}}>
-            <Image
-              style={{flex:1, width:'100%'}}
-              resizeMode='contain'
-              source={require('./img/drill-sgt-logo.gif')}
-            />
-          </View>
-          <View style={{flex: 5}}>
-            <Text style={{fontSize: 69, textAlign: 'left', paddingLeft: 10}}>Drill Sgt.</Text>
-          </View>
-        </View>
-
-        <View style={{flexGrow: 8}}>
-          <Text>Open up App.js to start working on your app!</Text>
-          <Text>Changes you make will automatically reload.</Text>
-          <Text>Shake your phone to open the developer menu.</Text>
-        </View>
-
-        <View style={{flexGrow: 1}}>
-          <Text>Bottom buttons</Text>
-        </View>
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit App.js
+        </Text>
+        <Text style={styles.instructions}>
+          {instructions}
+        </Text>
       </View>
     );
   }
@@ -35,8 +40,18 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
   },
 });
