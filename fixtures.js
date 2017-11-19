3ce61693712c6f50a3a1b8a1cede04e8d2296208
Conner D <connerdunn7399@gmail.com>
2017-11-18 23:24:24 -0700

    The fixtures.js is mostly working..

--- fixtures.js
+++ fixtures.js
@@ -3,6 +3,7 @@ var SQLite = require('react-native-sqlite-storage')
 var main = function () {
   var db = SQLite.openDatabase({name: 'my.db', location: 'default'}, successcb, errorcb);
   populateDatabase(db);
+  queryExercise(db);
 }
 
 var successcb = function () {
@@ -14,19 +15,38 @@ var errorcb = function () {
 }
 
 var populateDatabase = function (db) {
+  // Drop the tables
   var dropStmts = [
     "DROP TABLE IF EXISTS Exercise",
-    "DROP TABLE IF EXISTS Workout"
+    "DROP TABLE IF EXISTS Workout",
+    "DROP TABLE IF EXISTS Current"
   ];
   var bigStr = dropStmts.join(';\n');
+  db.executeSql(bigStr, [], successcb, errorcb);
 
-  database.executeSql(bigStr, [],
+  // Create the tables
+  var createStmt = [
+      "CREATE TABLE IF NOT EXISTS Exercise (name TEXT PRIMARY KEY NOT NULL, startCount INT NOT NULL)",
+      "CREATE TABLE IF NOT EXISTS Workout (id INT PRIMARY KEY NOT NULL, name TEXT NOT NULL, count INT NOT NULL, difficulty INT NOT NULL, time DATETIME NOT NULL, FOREIGN KEY (name) REFERENCES Exercise (name))",
+      "CREATE TABLE IF NOT EXISTS Current (id INT PRIMARY KEY NOT NULL, name TEXT NOT NULL, count INT NOT NULL, FOREIGN KEY (name) REFERENCES Exercise (name))"
+    ];
+  var bigStr = createStmt.join(';\n');
+  db.executeSql(bigStr, [], successcb, errorcb);
+
+  // Insert values
+  db.executeSql('INSERT INTO Exercise (name, startCount) VALUES ("Pushups", 10);', []);
+  db.executeSql('INSERT INTO Exercise (name, startCount) VALUES ("Situps", 20);', []);
+  db.executeSql('INSERT INTO Exercise (name, startCount) VALUES ("Diamond Pushups", 15);', []);
+}
+
+var queryExercise = function(db) {
+  db.executeSql('SELECT * FROM Exercise', [2],
     function () {
-      // db.transaction(queryEmployees,errorcb,function() {
-      //   console.log("Processing completed");
-      // });
+      console.log("select succeded", errorcb);
     },
     function (error) {
-      // TODO
+      console.log("select failed", error);
     });
-}
+};
+
+main();
