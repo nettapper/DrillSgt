1b4aaa3b30d3c52406e8c89c112c9d5125bff8b8
Conner D <connerdunn7399@gmail.com>
2017-11-19 00:28:19 -0700

    Added some default test data

--- fixtures.js
+++ fixtures.js
@@ -1,9 +1,12 @@
 var SQLite = require('react-native-sqlite-storage')
 
 var main = function () {
+  var testing = true;
   var db = SQLite.openDatabase({name: 'my.db', location: 'default'}, successcb, errorcb);
-  populateDatabase(db);
+  populateDatabase(db, testing);  // adds the Exercises, bool to say if testing
   queryExercise(db);
+  queryWorkout(db);
+  queryCurrent(db);
 }
 
 var successcb = function () {
@@ -14,35 +17,74 @@ var errorcb = function () {
   console.log("errorcb");
 }
 
-var populateDatabase = function (db) {
+var populateDatabase = function (db, testing) {
   // Drop the tables
-  var dropStmts = [
-    "DROP TABLE IF EXISTS Exercise",
-    "DROP TABLE IF EXISTS Workout",
-    "DROP TABLE IF EXISTS Current"
-  ];
-  var bigStr = dropStmts.join(';\n');
-  db.executeSql(bigStr, [], successcb, errorcb);
+  var dropStmts;
+  if(testing) {
+    console.log("Testing is true, dropping all tables");
+    db.executeSql("DROP TABLE IF EXISTS Workout", [], successcb, errorcb);
+    db.executeSql("DROP TABLE IF EXISTS Current", [], successcb, errorcb);
+  }
+  db.executeSql("DROP TABLE IF EXISTS Exercise", [], successcb, errorcb);
 
   // Create the tables
-  var createStmt = [
-      "CREATE TABLE IF NOT EXISTS Exercise (name TEXT PRIMARY KEY NOT NULL, startCount INT NOT NULL)",
-      "CREATE TABLE IF NOT EXISTS Workout (id INT PRIMARY KEY NOT NULL, name TEXT NOT NULL, count INT NOT NULL, difficulty INT NOT NULL, time DATETIME NOT NULL, FOREIGN KEY (name) REFERENCES Exercise (name))",
-      "CREATE TABLE IF NOT EXISTS Current (id INT PRIMARY KEY NOT NULL, name TEXT NOT NULL, count INT NOT NULL, FOREIGN KEY (name) REFERENCES Exercise (name))"
-    ];
-  var bigStr = createStmt.join(';\n');
-  db.executeSql(bigStr, [], successcb, errorcb);
+  db.executeSql("CREATE TABLE IF NOT EXISTS Exercise (name TEXT PRIMARY KEY NOT NULL, startCount INT NOT NULL)", [], successcb, errorcb);
+  db.executeSql("CREATE TABLE IF NOT EXISTS Workout (id INT PRIMARY KEY NOT NULL, name TEXT NOT NULL, count INT NOT NULL, difficulty INT NOT NULL, time DATETIME NOT NULL, completed BOOL NOT NULL, FOREIGN KEY (name) REFERENCES Exercise (name))", [], successcb, errorcb);
+  db.executeSql("CREATE TABLE IF NOT EXISTS Current (id INT PRIMARY KEY NOT NULL, name TEXT NOT NULL, count INT NOT NULL, FOREIGN KEY (name) REFERENCES Exercise (name))", [], successcb, errorcb);
 
   // Insert values
   db.executeSql('INSERT INTO Exercise (name, startCount) VALUES ("Pushups", 10);', []);
   db.executeSql('INSERT INTO Exercise (name, startCount) VALUES ("Situps", 20);', []);
   db.executeSql('INSERT INTO Exercise (name, startCount) VALUES ("Diamond Pushups", 15);', []);
+
+  if(testing) addExampleData(db);
+}
+
+var addExampleData = function(db) {
+  // Insert values
+  db.executeSql('INSERT INTO Workout (id, name, count, difficulty, time, completed) VALUES (1, "Pushups", 10, 1, "2004-01-01 02:34:56", 1);', []);
+  db.executeSql('INSERT INTO Workout (id, name, count, difficulty, time, completed) VALUES (2, "Pushups", 11, 1, "2004-01-02 02:34:56", 1);', []);
+  db.executeSql('INSERT INTO Workout (id, name, count, difficulty, time, completed) VALUES (3, "Situps", 20, 3, "2004-01-02 05:34:56", 1);', []);
+  db.executeSql('INSERT INTO Workout (id, name, count, difficulty, time, completed) VALUES (4, "Diamond Pushups", 15, 5, "2004-01-02 05:34:56", 0);', []);
+  db.executeSql('INSERT INTO Workout (id, name, count, difficulty, time, completed) VALUES (5, "Pushups", 20, 4, "2004-03-02 05:34:56", 1);', []);
+
+  db.executeSql('INSERT INTO Current (id, name, count) VALUES (10, "Pushups", 21);', []);
 }
 
 var queryExercise = function(db) {
   db.executeSql('SELECT * FROM Exercise', [],
     function (data) {
-      console.log("select succeded");
+      console.log("select Exercise succeded");
+      if(data && data["rows"] && data["rows"]["length"]) {
+        for(var i=0; i < data["rows"]["length"]; i++) {
+          console.log(data["rows"]["item"](i));
+        }
+      }
+    },
+    function (error) {
+      console.log("select failed", error);
+    });
+};
+
+var queryWorkout = function(db) {
+  db.executeSql('SELECT * FROM Workout', [],
+    function (data) {
+      console.log("select Workout succeded");
+      if(data && data["rows"] && data["rows"]["length"]) {
+        for(var i=0; i < data["rows"]["length"]; i++) {
+          console.log(data["rows"]["item"](i));
+        }
+      }
+    },
+    function (error) {
+      console.log("select failed", error);
+    });
+};
+
+var queryCurrent = function(db) {
+  db.executeSql('SELECT * FROM Current', [],
+    function (data) {
+      console.log("select Current succeded");
       if(data && data["rows"] && data["rows"]["length"]) {
         for(var i=0; i < data["rows"]["length"]; i++) {
           console.log(data["rows"]["item"](i));
