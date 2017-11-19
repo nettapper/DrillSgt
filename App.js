2eb2601980a0edf75a4dbcdcefbdb353fab10228
Conner D <connerdunn7399@gmail.com>
2017-11-18 20:32:16 -0700

    This chart is good enough for now!

--- App.js
+++ App.js
@@ -10,6 +10,7 @@ import {
   StyleSheet,
   Text,
   View,
+  ScrollView,
   Image,
   Button
 } from 'react-native';
@@ -27,48 +28,53 @@ const instructions = Platform.select({
 export default class App extends Component<{}> {
   render() {
     return (
-      <View style={styles.container}>
-        <View style={{marginTop: 30, flexDirection: 'row'}}>
-          <View style={{flex: 1, padding: 5}}>
-            <Image
-              style={{flex:1, width:'100%'}}
-              resizeMode='contain'
-              source={require('./img/drill-sgt-logo.gif')}
+      <ScrollView>
+        <View style={styles.container}>
+          <View style={{marginTop: 30, flexDirection: 'row'}}>
+            <View style={{flex: 1, padding: 5}}>
+              <Image
+                style={{flex:1, width:'100%'}}
+                resizeMode='contain'
+                source={require('./img/drill-sgt-logo.gif')}
+              />
+            </View>
+            <View style={{flex: 5}}>
+              <Text style={{fontSize: 69, textAlign: 'left', paddingLeft: 10}}>Drill Sgt.</Text>
+            </View>
+          </View>
+
+          <View style={{flexGrow: 2}}>
+            <PieChart/>
+          </View>
+
+          <View style={{flexGrow: 6}}>
+            <Text style={styles.welcome}>
+              Welcome to React Native!
+            </Text>
+            <Text style={styles.instructions}>
+              To get started, edit App.js
+            </Text>
+            <Text style={styles.instructions}>
+              {instructions}
+            </Text>
+          </View>
+
+          <View style={{flexGrow: 1}}>
+            <Button
+              onPress={onPressCompleteWorkout}
+              title="Complete This Workout"
+              color="#841584"
+              accessibilityLabel="Once finished the workout you will input your ratings."
+            />
+            <Button
+              onPress={onPressStatistics}
+              title="Stats"
+              color="#66CCCC"
+              accessibilityLabel="Statistics and charts."
             />
           </View>
-          <View style={{flex: 5}}>
-            <Text style={{fontSize: 69, textAlign: 'left', paddingLeft: 10}}>Drill Sgt.</Text>
-          </View>
         </View>
-
-        <View style={{flexGrow: 8}}>
-          <Text style={styles.welcome}>
-            Welcome to React Native!
-          </Text>
-          <Text style={styles.instructions}>
-            To get started, edit App.js
-          </Text>
-          <Text style={styles.instructions}>
-            {instructions}
-          </Text>
-          <PieChart/>
-        </View>
-
-        <View style={{flexGrow: 1}}>
-          <Button
-            onPress={onPressCompleteWorkout}
-            title="Complete This Workout"
-            color="#841584"
-            accessibilityLabel="Once finished the workout you will input your ratings."
-          />
-          <Button
-            onPress={onPressStatistics}
-            title="Stats"
-            color="#66CCCC"
-            accessibilityLabel="Statistics and charts."
-          />
-        </View>
-      </View>
+      </ScrollView>
     );
   }
 }
--- components/PieChart.js
+++ components/PieChart.js
@@ -5,12 +5,23 @@ import {
   View
 } from 'react-native';
 
+import { VictoryPie, VictoryTheme } from "victory-native";
+
 export default class App extends Component<{}> {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>TEST FROM PIE CHART</Text>
-      </View>
+      <VictoryPie
+        responsive={true}
+        innerRadius={100}
+        padAngle={3}
+        colorScale={["tomato", "cyan"]}
+        data={[
+          { x: "Failed", y: 24 },
+          { x: "Complete", y: 78 }
+        ]}
+        labelRadius={90}
+        style={{ labels: { fill: "black", fontSize: 20, fontWeight: "bold" } }}
+      />
     );
   }
 }
